9f1df2855ea96ec052abb576de3a73fe50f1bc4c
KUNYMANN <kunymann@hotmail.com>
2024-06-10 16:22:17 -0300

    terminado

--- EJERCICIOS y PRACTICAS/ejercicios.py
+++ EJERCICIOS y PRACTICAS/ejercicios.py
@@ -2,9 +2,16 @@ nombre="Santiago"
 saludo="Hola Santiago "  +  "buen dia!!"
 print(saludo)
 
+#IMPRIMIR con un solo print
+print("como estan") # linea 1                se puede pedir que imprima en dos lineas de esta manera
+print("manga de genios") # linea 2           ejemplo print linea 1 y print linea 2 o simplemente 
+print("como estan\nmanga de genios") #       colocando "n\"" el resultado es igual, lo va a imprimir en dos lineas con un solo print, 
+
+#IMPRIMIR con un varios print en una misma linea
 cancion_de_cuna="Arroro mi Niño"
 artista_compositor=" de Joan Manuel Serrat " "El Catalan"
-print(cancion_de_cuna + artista_compositor); print(saludo) #de esta manera con el ";" se puede colocar otra orden "print" que lo va a imprimir en el renglon siguiente
+print(cancion_de_cuna + artista_compositor); print(saludo) #de esta manera con el ";" se puede colocar otra orden "print" 
+                                                           #que lo va a imprimir en el renglon siguiente
 
 # IDENTAR significa dejar cuatro espacios con el espaciador o un tablulado desde el borde izquierdo 
 a=0
