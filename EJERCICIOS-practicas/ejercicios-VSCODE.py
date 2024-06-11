95d31a078ce7a46f98c85af20b92863a0f59d730
KUNYMANN <kunymann@hotmail.com>
2024-06-11 14:31:27 -0300

    terminado

--- EJERCICIOS-practicas/ejercicios-VSCODE.py
+++ EJERCICIOS-practicas/ejercicios-VSCODE.py
@@ -1,1 +1,6 @@
-# TIPADO DINAMICO
+# TIPADO DINAMICO: es que a una variable se le puede asignar valores de diferentes tipos (number o string) en diferentes momentos del programa
+valor=10 
+print(valor)
+
+valor="Kunymann" 
+print(valor)
