d0b381f8413b7b321bc7e335f7e4060d7e2c66b9
KUNYMANN <kunymann@hotmail.com>
2024-06-11 20:03:04 -0300

    terminado

--- EJERCICIOS y PRACTICAS/3-ejercicios operadores logicos.py
+++ EJERCICIOS y PRACTICAS/3-ejercicios operadores logicos.py
@@ -1,24 +1,3 @@
-# Prioridadde los operadores logicos
-# 1. not lo contrario a lo evaluado, la negacion invierte el resultado si es true pasa a ser false y si es false pasa a ser true
-# 2. and componente que da una orden que se tienen que cumplir varias ordenes  para ver si se cumple una cosa "y" tambien laotra 
-# (en AND para que el resultado sea verdadero todo debe resultar verdadero)
-# 3. or componente que evalua si es esto "o" lo otro (en OR para que el resultado sea verdadero, basta con que uno sea verdadero)
-
-# EJEMPLO
-# a=10
-# b=12
-# c=13
-# d=13
-#      desarrollo sobre un ejercicio con tres tipos de operadores:logicos, aritmeticos y relacionales
-
-#   ((a>b)or(a<c))     and     ((a==c)or(a>=b)
-# ((10>12)or(10<13))   and   ((10==13)or(10>=12)
-#     (F  or  T)                 (F   or   F)
-#        (T)           and             (F)
-#                   (T and F)
-#                      (F)  resultado final False
-
-
 
 # EJERCICIO CON APLICACION DE OPERADORES 
 a=10
