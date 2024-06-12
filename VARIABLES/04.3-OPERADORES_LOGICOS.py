1ecc3bba2be0d2f6caa476b24b5d9693fa711849
KUNYMANN <kunymann@hotmail.com>
2024-06-12 01:13:39 -0300

    terminado

--- VARIABLES/04.3-OPERADORES_LOGICOS.py
+++ VARIABLES/04.3-OPERADORES_LOGICOS.py
@@ -2,26 +2,29 @@
 # and.... conjuncion o multiplicacion logica true es 1 y false es 0  
 # or..... disyuncion o suma logica
 # not.... negacion      
+'''PRIORIDAD DE LOS PERADORES LOGICOS
+not
+and y
+or'''
+
 
 """OPERADOR AND "multiplicacion logica" con un minimo de dos operandos de valores logicos o booleanos a comparar
-
 En la programacion TRUE tiene el valor de 1 dado que solo el 1 multiplicado por si mismo da 1
 y FALSE tiene el valor 0 porque todo numero multiplicado por 0 es siempre 0
-
 operando1   operador   operando2   RESULTADO   
 True           and        True       TRUE  (solo se da si ambos operandos son verdaderos el resultado siempre sera true) 1 
 True           and        False      FALSE (si cuaquiera de los dos operandos es falso el resultado siempre sera false)  0 
 False          and        True       FALSE (si cuaquiera de los dos operandos es falso el resultado siempre sera false)  0 
 False          and        False      FALSE (si cuaquiera de los dos operandos es falso el resultado siempre sera false)  0 
 
-OPERADOR OR
+OPERADOR OR se lo conoce como una suma logica
 operando  RESULTADO
 True           or        True       TRUE (basta que cualquiera de los dos operandos sea verdadero el resultado siempre sera true) 1
 True           or        False      TRUE (basta que cualquiera de los dos operandos sea verdadero el resultado siempre sera true) 1
 False          or        True       TRUE (basta que cualquiera de los dos operandos sea verdadero el resultado siempre sera true) 1
 False          or        False      FALSE (cuando ambos operandos son falso el resultado siempre sera false) 0
 
-OPERADOR NOT
+OPERADOR NOT se lo conoce como un valor de negacion
 operando    RESULTADO
 not(True)    FALSE  (UNA VERDAD NEGADA ES UNA MENTIRA) Ejemplo: #mentira (jesus sí existió)...FALSO porque se esta mintiendo sobre una verdad jesus existio
 not(False)   TRUE   (UNA MENTIRA NEGADA ES UNA VERDAD) Ejemplo: #mentira (jesus no existio)...VERDAD porque jesus existio"""
@@ -44,10 +47,7 @@ print(not(edad>56))#(no es cierto que 54 sea mayor que 56? True es verdad porque
 # 3. or componente que evalua si es esto "o" lo otro (en OR para que el resultado sea verdadero, basta con que uno sea verdadero)
 
 # EJEMPLO
-# a=10
-# b=12
-# c=13
-# d=13
+# a=10, b=12, c=13, d=13
 #      desarrollo sobre un ejercicio con tres tipos de operadores:logicos, aritmeticos y relacionales
 
 #   ((a>b)or(a<c))     and     ((a==c)or(a>=b)
