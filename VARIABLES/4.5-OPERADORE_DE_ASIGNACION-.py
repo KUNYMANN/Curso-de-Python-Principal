411fdb7e2692c9e680a1fb77d3a40d156ab9883b
KUNYMANN <kunymann@hotmail.com>
2024-06-12 14:48:20 -0300

    terminado

--- VARIABLES/4.5-OPERADORE_DE_ASIGNACION-.py
+++ VARIABLES/4.5-OPERADORE_DE_ASIGNACION-.py
@@ -1,8 +1,17 @@
-a=0     # esto es igual a
-a=a+5
-print(a)
 # LOS VALORES DE ASIGNACION SIRVEN PARA CORTAR EL CODIGO
-a=0     # esto
+
+'''para poder usar operadores de asignacion previamente debemos declarar
+una variable y asignado un valor inicial, porque estos solo sirven para
+aumentar, disminuir, dividir, multiplicar a lo que ya tenemos'''
+
+'''AQUI EN EL EJEMPLO VEMOS UNA VARIABLE CON UN VALOR ASIGNADO, LOS OPERADORES DE ASIGNACION 
+VAN A UTILIZAR A ESTA PARA APLICAR EL RESTO DE LAS FUNCIONES QUE HARA QUE AUMENTE O DISMINUYA
+SEGUN LE QUE LE ASIGNEMOS'''
+a=2   #VARIABLE CON SU VALOR ASIGNADO
+a=a+5 # esta es la forma larga de escribir una operacion de asignacion
+print(a)
+
+a=2   # a continuacion utilizamos los metodos acortados de los operadores de asignacion
 a+=5  #suma en asignacion
 print(a)
 a-=2  #resta en asignacion
@@ -13,5 +22,14 @@ a/=3 #division en asignacion
 print(a)
 a**=2 #potenciacion en asignacion
 print(a)
-a%=2 #modulo en asignacion
+a%=2 #modulo en asignacion da como resultado el resto de la division
+print(a)
+
+a=2    # VARIAVLE CON SU VALOR ASIGNADO
+a+=5   #suma en asignacion
+a-=2   #resta en asignacion
+a*=3   #multiplicacion por asignacion
+a/=3   #division en asignacion
+a**=2  #potenciacion en asignacion
+a%=2   #modulo en asignacion da como resultado el resto de la division
 print(a)
