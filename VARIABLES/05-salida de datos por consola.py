82cebb39624141d128bbfb58c881e4fbbbb3429c
KUNYMANN <kunymann@hotmail.com>
2024-06-12 15:11:48 -0300

    terminado

---
+++ VARIABLES/05-salida de datos por consola.py
@@ -0,0 +1,17 @@
+
+"MANERAS DE REALIZAR SALIDAS DE DATOS"
+
+#1
+nombre="Hector"
+edad=58
+print("Hola",nombre,"tienes",edad,"años")
+
+#2
+nombre="Hector"
+edad=58
+print("Hola {} tienes {} años".format(nombre,edad))
+
+#3
+nombre="Hector"
+edad=58
+print(f"Hola {nombre} tienes {edad} años")
