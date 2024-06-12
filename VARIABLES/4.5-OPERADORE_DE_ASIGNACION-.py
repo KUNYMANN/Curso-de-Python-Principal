c2d6276423a7f601b4a86037a987d59507c2fea5
KUNYMANN <kunymann@hotmail.com>
2024-06-12 01:33:41 -0300

    terminado

---
+++ VARIABLES/4.5-OPERADORE_DE_ASIGNACION-.py
@@ -0,0 +1,17 @@
+a=0     # esto es igual a
+a=a+5
+print(a)
+# LOS VALORES DE ASIGNACION SIRVEN PARA CORTAR EL CODIGO
+a=0     # esto
+a+=5  #suma en asignacion
+print(a)
+a-=2  #resta en asignacion
+print(a)
+a*=3 #multiplicacion por asignacion
+print(a)
+a/=3 #division en asignacion
+print(a)
+a**=2 #potenciacion en asignacion
+print(a)
+a%=2 #modulo en asignacion
+print(a)
