c6db55a73fc607a95e89020b10fa3fb99edad2b4
KUNYMANN <kunymann@hotmail.com>
2024-06-12 14:54:59 -0300

    terminado

--- VARIABLES/4.5-OPERADORE_DE_ASIGNACION-.py
+++ VARIABLES/4.5-OPERADORE_DE_ASIGNACION-.py
@@ -1,12 +1,14 @@
 # LOS VALORES DE ASIGNACION SIRVEN PARA CORTAR EL CODIGO
 
-'''para poder usar operadores de asignacion previamente debemos declarar
+'''para poder usar operadores de asignacion previamente es fundamental declarar
 una variable y asignado un valor inicial, porque estos solo sirven para
-aumentar, disminuir, dividir, multiplicar a lo que ya tenemos'''
+aumentar, disminuir, dividir, multiplicar a lo que ya tenemos, en caso de no 
+hacerlo python no va a saber a quien aplicarle dicho valor asignado al operador
+y generara un error'''
 
 '''AQUI EN EL EJEMPLO VEMOS UNA VARIABLE CON UN VALOR ASIGNADO, LOS OPERADORES DE ASIGNACION 
-VAN A UTILIZAR A ESTA PARA APLICAR EL RESTO DE LAS FUNCIONES QUE HARA QUE AUMENTE O DISMINUYA
-SEGUN LE QUE LE ASIGNEMOS'''
+VAN A UTILIZAR A ESTA VARIABLE PARA APLICAR EL RESTO DE LAS FUNCIONES PARA QUE AUMENTE O DISMINUYA
+SEGUN EL OPERADOR ARITMETICO QUE LE ASIGNEMOS'''
 a=2   #VARIABLE CON SU VALOR ASIGNADO
 a=a+5 # esta es la forma larga de escribir una operacion de asignacion
 print(a)
