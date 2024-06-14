83a3b41c6247ff906e5b27ae2c890e68b444f57e
KUNYMANN <kunymann@hotmail.com>
2024-06-14 20:16:31 -0300

    terminado

---
+++ EJERCICIOS y PRACTICAS/0-ejercicios.py
@@ -0,0 +1,11 @@
+# escribir la siguiente expresion algotitmica
+#  a3x(b2-2ac)
+#     2b
+
+a=float(input("a -> "))
+b=float(input("b -> "))
+c=float(input("c -> "))
+ejercicio1= (a**3*(b**2-2*a*c))/(2*b)
+print("El resultado es:", ejercicio1, "exelente")
+
+print(f"el resultado es {ejercicio1} exelente!!!")
