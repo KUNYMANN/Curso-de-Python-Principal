44dc5f8d462610417871f7352123088ac8d7e962
KUNYMANN <kunymann@hotmail.com>
2024-06-16 23:22:03 -0300

    terminado

--- EJERCICIOS y PRACTICAS/0.1-ejercicios 2- ELEMENTOS BASICOS.py
+++ EJERCICIOS y PRACTICAS/0.1-ejercicios 2- ELEMENTOS BASICOS.py
@@ -1,7 +1,7 @@
 #DETERMINAR LA SOLUCION LOGICA DE LA SIGUIENTE OPERACION
 # Valores para a=10 y b=5 
 #((3+5X8)<3 and ((-6/3x4)+2<2)) or (a>b)  primero se resuelven los parentesis de los mas internos a los mas externos
-#  ( 43  <3 and      (-8 +2<2)) or  True  luego los siguientes parentesis internos 
+#  ( 43  <3 and    ( -8+2  <2)) or  True  luego los siguientes parentesis internos 
 #    (43<3  and        -6<2   ) or  True  seguimos resolviendo lo terminos entre parentesis
 #    (43<3  and        True   ) or  True  seguimos resolviendo lo terminos entre parentesis
 #    (False and        True   ) or  True  resultados booleanos
