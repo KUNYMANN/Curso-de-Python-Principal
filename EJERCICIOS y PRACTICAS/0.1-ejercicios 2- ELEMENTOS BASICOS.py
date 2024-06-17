1efa01a2c350be6c4d0f4ac125c4dbc5994986b3
KUNYMANN <kunymann@hotmail.com>
2024-06-16 22:58:06 -0300

    terminado

---
+++ EJERCICIOS y PRACTICAS/0.1-ejercicios 2- ELEMENTOS BASICOS.py
@@ -0,0 +1,7 @@
+#DETERMINAR LA SOLUCION LOGICA DE LA SIGUIENTE OPERACION
+#((3+5X8)<3and((-6/3x4)+2<2))or(a>b)
+a=float(input("ingrese aqui un valor para a: "))
+b=float(input("ingrese aqui un valor para b: "))
+ejercicio2=((3+5*8)<3 and ((-6/3*4)+2<2)) or (a>b)
+print(f"el resultado del ejercicio es:  {ejercicio2} maravilloso!!")
+print("el resultado del ejercicio es:" , ejercicio2, "maravilloso!!")
