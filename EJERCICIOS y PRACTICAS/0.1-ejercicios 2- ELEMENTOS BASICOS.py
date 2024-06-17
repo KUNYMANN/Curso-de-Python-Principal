69f6d517578e5d5d747656e948ac8e70c38b3c29
KUNYMANN <kunymann@hotmail.com>
2024-06-16 23:20:03 -0300

    trerminado

--- EJERCICIOS y PRACTICAS/0.1-ejercicios 2- ELEMENTOS BASICOS.py
+++ EJERCICIOS y PRACTICAS/0.1-ejercicios 2- ELEMENTOS BASICOS.py
@@ -1,5 +1,13 @@
 #DETERMINAR LA SOLUCION LOGICA DE LA SIGUIENTE OPERACION
-#((3+5X8)<3and((-6/3x4)+2<2))or(a>b)
+# Valores para a=10 y b=5 
+#((3+5X8)<3 and ((-6/3x4)+2<2)) or (a>b)  primero se resuelven los parentesis de los mas internos a los mas externos
+#  ( 43  <3 and      (-8 +2<2)) or  True  luego los siguientes parentesis internos 
+#    (43<3  and        -6<2   ) or  True  seguimos resolviendo lo terminos entre parentesis
+#    (43<3  and        True   ) or  True  seguimos resolviendo lo terminos entre parentesis
+#    (False and        True   ) or  True  resultados booleanos
+#                        False  or  True  resultados booleanos
+#                              True       resultado booleanos
+
 a=float(input("ingrese aqui un valor para a: "))
 b=float(input("ingrese aqui un valor para b: "))
 ejercicio2=((3+5*8)<3 and ((-6/3*4)+2<2)) or (a>b)
