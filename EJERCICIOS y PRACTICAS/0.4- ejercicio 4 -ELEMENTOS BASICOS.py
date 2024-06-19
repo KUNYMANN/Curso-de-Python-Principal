18ae2897c1c372d4cb649c5deb01878059862b55
KUNYMANN <kunymann@hotmail.com>
2024-06-19 15:11:12 -0300

    terminado

--- EJERCICIOS y PRACTICAS/0.4- ejercicio 4 -ELEMENTOS BASICOS.py
+++ EJERCICIOS y PRACTICAS/0.4- ejercicio 4 -ELEMENTOS BASICOS.py
@@ -4,6 +4,12 @@ radio=float(input("coloqe el valor del radio: "))
 area=(math.pi*radio**2)
 longitud=(2*math.pi*radio)
 
+print("el valor del area es:" , area)                            
+print(f"el valor del area es: {area}")
+print(f"el valor del area es: {area:.2f}") #redondeo 2 decimales
+print("el valor del area es: %.2f"% area) #redondeo 2 decimales
 
-print("el valor del area es:", area)
-print("la longitud de la circunferencia es:", longitud)
+print("la longitud de la circunferencia es:" ,longitud)
+print(f"la longitud de la circunferencia es: {longitud}")
+print(f"la longitud de la circunferencia es: {longitud:.2f}") #redondeo 2 decimales
+print("la longitud de la circunferencia es: %.2f" % longitud) #redondeo 2 decimales
