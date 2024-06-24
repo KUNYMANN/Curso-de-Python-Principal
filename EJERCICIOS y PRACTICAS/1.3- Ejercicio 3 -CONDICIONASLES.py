e4ddaa855721f02d7d79fade976960be9b2846fa
KUNYMANN <kunymann@hotmail.com>
2024-06-24 14:36:36 -0300

    terminado

--- EJERCICIOS y PRACTICAS/1.3- Ejercicio 3 -CONDICIONASLES.py
+++ EJERCICIOS y PRACTICAS/1.3- Ejercicio 3 -CONDICIONASLES.py
@@ -4,3 +4,12 @@ if letra=="a" or letra=="e" or letra=="i" or letra=="o" or letra=="u"  :
     print("Es una vocal") #entonces si alguna de las condiciones se cumple el resulta do a imprimir sera "Es una Vocal"
 else: #caso contrario(=else) si se coloca cualquier caracter que no es una Vocal
     print("No es una vocal") #va a arrojar como resultado "No es una Vocal"
+
+
+
+letra=input("Ingrese un caracter->  ").upper()
+if letra=="A" or letra=="E" or letra=="I" or letra=="O" or letra=="U"  :
+#(if=)SI la letra es igual a "a" (or=)o la letra es igual a "e" (or=)o la letra es igual a "i" (or=)o la letra es igual a "o" (or=)o la letra es igual a "u" 
+    print("Es una vocal") #entonces si alguna de las condiciones se cumple el resulta do a imprimir sera "Es una Vocal"
+else: #caso contrario(=else) si se coloca cualquier caracter que no es una Vocal
+    print("No es una vocal") #va a arrojar como resultado "No es una Vocal"
