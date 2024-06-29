d7adc8ef3b3b9fa3283c0c806ef8d2fcb9da0da4
KUNYMANN <kunymann@hotmail.com>
2024-06-29 01:39:13 -0300

    terminado

---
+++ EJERCICIOS y PRACTICAS/0.0-EJERCICIOS1 (cadenas).py
@@ -0,0 +1,12 @@
+#Ejercico 1
+'''Escribir un programa que pregunte el nombre del usuario en la consola y un número entero 
+e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.'''
+#con un BUCLE for i in range,  recordar que el PRINT va identado
+'''nombre=input("Coloque su nombre: ")
+num=int(input("Coloque un numero de cantidades de veces que aparecera su nombre: "))
+for i in range(num):
+    print(nombre)'''
+#con \n.join 
+'''nombre=input("Coloque su nombre: ")
+num=int(input("Coloque un numero de cantidades de veces que aparecera su nombre: "))
+print("\n".join(nombre)*num)'''
