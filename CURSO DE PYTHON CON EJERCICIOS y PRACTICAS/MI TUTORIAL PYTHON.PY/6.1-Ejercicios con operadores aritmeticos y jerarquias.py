7e70caefebdca4310753c24d50877280b237411f
KUNYMANN <kunymann@hotmail.com>
2024-08-24 04:46:40 -0300

    Terminado

---
+++ CURSO DE PYTHON CON EJERCICIOS y PRACTICAS/MI TUTORIAL PYTHON.PY/6.1-Ejercicios con operadores aritmeticos y jerarquias.py
@@ -0,0 +1,10 @@
+'UN OPERADOR ARITMETICO VA A SER LA REPRESENTACION SIMBOLICA DE UN PROCEDIMIENTO MATEMATICO'
+print(2+3) #    5  #operador suma (muestra el resultado de la suma)
+print(7-3) #    4   #operador resta (muestra el resultado de la resta)
+print(6*3) #   18   #operador multiplicacion (muestra el resultado de la multiplicacion)
+print(89/7) #  12.714285  #operador de division simple (muestra el cociente con decimales)
+print(89//7) # 12  #operador division entera (muestra el cociente entero, sin decimales)
+print(13%4) #   1  #operador modulo (muestra el resto de una division, inclusive si es 0)
+print(4**3) #  64  #operador de exponenciacion (muestra el resultado de la multiplicacion del numero tantas veces el exponente)
+
+#OPERADORES ARITMETICOS CON VARIABLES
