80de147429598cd9c44002969c3bed46dc816c8a
KUNYMANN <kunymann@hotmail.com>
2024-08-25 19:16:05 -0300

    terminado

---
+++ CURSO DE PYTHON CON EJERCICIOS y PRACTICAS/MI TUTORIAL PYTHON.PY/String - cadenas de textos.py
@@ -0,0 +1,69 @@
+#CADENAS DE TEXTO (STRINGS)
+'Una cadena siempre va a presentar METODOS, estos son otras funionalidades que se le pueden dar a las cadenas'
+#QUE ES UNA CADENA O STRING QUE ES LO MISMO?
+'''Es una secuencia de caracteres, o un caracter es una letra y cuando tomamos varias letras,
+varios caracteres y los ponemos en secuencia, ya sea logica o ilogicamente, entonces
+ahi se forman las cadenas o strings'''
+#el hecho que tengamos cadenas no quiere decir que especificamente tengamos que llevar
+#un orden logico
+'''una cadena puede ser "QWERTY" que son las primeras letras de arriba del teclado y eso no tendria sentido 
+pero no deja de ser una cadena, lo mismo ocurre para la palabra, mensaje, o la palabra cadena'''
+#UNA CADENA NO NECESARIAMENTE TIENE QUE SER UN MENSAJE LOGICO, SOLO NECESITA UNA SUCECION DE CARACTERES
+
+#Ejemplo
+"M  O  N  T  Y     P  Y  T  H  O  N"  #CADA CARACTER TIENE UN ESPACIO ASIGNADO EN LA CADENA
+'0  1  2  3  4  5  6  7  8  9  10 11' # como podemos verlo aqui 
+# Ahora bien estos espacios son importantes tenerlos presente, porque las cadenas van a ocupar un espacio, 
+# pero a su vez cada caracter dentro de la cadena va a ocuar otro espacio diferente al de la cadena
+'esto quiere decir que si yo mando a llamar al espacio 4 me va a mostar Y de MONTY'
+#en algunos casos la cuenta es asi: 0 1 2 3, empezando por el 0 la primer letra, 
+# en otros la cuenta no es asi, la cuenta es 1 2 3 empezando por la M que es la primera de la palabra MONTY
+# Y en otros el 0 va a ser la comilla, que no se cuenta pero hay algunas excepciones que si
+#fundamental tener presente siempre que 'CADA CARACTER OCUPA UN ESPACIO EN LA CADENA'
+
+"DEBANADO DE CADENAS"
+# la mayoria de los lenguajes de programacion permiten el debanado de cadenas JavaScript, Python, Siyar
+# y se los identifica con un o varios slash \ invertido
+'conciste en mostrar contenido de una cadena de manera segmentada' 
+#que quiere decir, que nosotros por medio de instrucciones predeterminadas,
+# solo vamos a mostrar un espacio de la cadena, solo vamos a mostrar un conjunto de letras
+# solo vamos a mostrar unas cuantas palabras, a eso se lo llama DEBANADO DE CADENAS.
+'cuando nosotros queremos mostrar una cadena, pero solo por segmentos, solo por fragmentos de la misma'
+# Ejenplo de DEBANADO
+"M  O  N  T  Y     P  Y  T  H  O  N" 
+'0  1  2  3  4  5  6  7  8  9  10 11'
+
+# ESTO ASI NO SE VA A APLICAR EN TODOS LOS LENGUAJES DE PROGRAMACION
+#vamos a ver en que momento se ocupa una regla, en que momento se ocupa otro debanado, en 
+#que momento se ocupa un debanado diferente y asi.
+
+cadena='MONTY PYTHON'
+print(cadena[0:5])
+#[0:5] = "MONTY " #yo aqui mande a llamar del 0 al 5 y me devuelve MONTY 
+' " M O N T Y " '
+# 0 1 2 3 4 5 #porque de 0 a 5 esta MONTY, vemos que toma la comilla como indice 0
+print(cadena[:3])
+#[ :3] = "MON "   #espacio null me va a traer, las primeras 3 letras o sea MON
+' " M O N " ' # lo mismo ocurre aqui toma como indice 0 la comilla
+# 0 1 2 3
+print(cadena[-3:])
+#[-3: ] = "HON"   #luego del 3 : espacio en blanco eso me va a traer HON, este es un tipo de debanado
+'" H  O  N " '    #diferente , este lo que me va a traer es las ultimas tres letras de la cadena puesto que 
+# -1 -2 -3        #la solicitud se hizo con un numero negativo, comienza a contar de atras para adelante
+print(cadena[:-3])
+#[ :-3] = "MONTY PYT" aqui esta eliminado de la seleccion las tres ultimas letras ya que pidio que muestre desde el caracter 0 al -3
+'"M  O  N  T  Y     P  Y  T  " '
+# 0  1  2  3  4  5  6  7  8
+print(cadena[0:11])
+#[0:11] = "MONTY PYTO"
+' " M  O  N  T  Y     P  Y  T  H  O  "  ' #aqui en la solicitud ocurren dos cosas
+' 0 1  2  3  4  5  6  7  8  9  10 11  ' # que tome el valor inicial 0 para la comilla entonces el 11 seria la "O" de PITHON " M  O  N  T  Y    P  Y  T  H  O  " 
+print(cadena[0:12])
+#[0:12] = "MONTY PYTON"
+' " M  O  N  T  Y     P  Y  T  H  O  N"  ' # si queremos que nos devuleva la cadena entera debemos agregar un valor mas al indice, porque si colocamos el ultimo
+' 0 1  2  3  4  5  6  7  8  9 10 11 12'  # indice de la cadena puede que el sistema tome como indice 0 las comillas, entonces no nos incluira el ultimo indice
+
+### ATENCION A TENER SIEMPRE EN CUENTA
+"""cuando uno solicita un determinado numero de caracteres de una cadena, si se tomo el primero no incluye el ultimo, dado que la solicitud se hace
+hasta tal numero, NO INCLUSIVE, si queremos que lo incluya a este ultimo,  debemos solicita un caracter mas, como cuando contamos con los dedos"""
+'no ocurre esto a la inversa, cuando la solicitud la hacemos con numeros negativos'
---
+++ VARIABLES/00-TODO STRING.py/03-Strings.py
@@ -0,0 +1,44 @@
+texto="Hola Mundo" #dato STRING
+#      0123456789 cada caracter dentro de un dato tiene asignado un indice y este comienza desde 0 
+#      tambien se cuenta como caracter a los espacios vacios
+print(texto) #con Shift + Alt mas flecha hacia abajo se duplica la linea 
+print(texto.upper()) #si elegimos el metodo UPPER()automaticamente pasa todo el texto a MAYUSCULAS
+print(texto.lower()) #si elegimos el metodo LOWER()automaticamente pasa todo el texto a minusculas
+print(texto.title()) # si elegimo el metodo TITLE()automaticamente pasa a mayuscula las primera letra de cada palabra Del Texto
+print(texto.find("M")) #si elegimos el metodo FIND("")automaticamente busca en que INDICE esta colocado el caracter solicitado
+print(texto.find("Mun")) #si elegimos el metodo FIND("")automaticamente busca en que INDICE esta colocado el 1er. caracter solicitado
+print(texto.find("mun")) #si elegimos el metodo FIND("")automaticamente busca en que INDICE esta colocado el 1er. caracter solicitado, ATENCION!!! que
+# si colocamos mal el texto va a dar error (-1) porque la busqueda se realiza colocando exactamente como esta escrito, respetando si esta en mayuscula o minuscula
+correo=input("Ingrese su correo electronico: ") 
+print(correo[:correo.find("@")] + "@ceu.es.com") #de esta manera find busca el indice en que esta ubicado el caracter solicitado "@" y a partir de ahi 
+                                                 # con el + se ejecuta en consola el reemplazo por el texto que querramos introducir
+precio = input("Introduce el precio del producto con dos decimales: ")
+print(precio[:precio.find(".")], "euros y", precio[precio.find(".")+1:], "centimos.") # aqui toma en el valor de la variable "precio" 
+#desde el indice 0 por eso no hay nada escrito antes de los ":"  hasta el punto (.) en el valor colocado, ya que pide un valor con decimales, y lo separa
+#en dos cadenas. que nos permiten introducir el "string" que querramos, asi que incertamos lo que queremos que aparezca en consola en este caso "euros y" 
+# despues colocamos "," para concatenar con la segunda parte del valor que resta a partir del punto (.) por eso se pide del valor introducido en la variable 
+# "precio" a partir del (.) en adelante por eso se de ahi que se coloca "+ 1" lo cual sera el siguiente indice que quiero utilizar, 
+# si coloco los ":" sin nada detras quiere decir que es hasta el final de la variable que va a imprimir la consola
+frase=input("Ingrese una frase: ")
+print(frase[::-1]) #colocando de esta manera se invierte la frase letra por letra o lo que se haya ingresado como valor en la variante                                         
+
+print(texto.replace("Mun","Kuny//"))#de esta manera se reemplaza al nuevo texto o parte del nuevo texto
+nuevo_texto=texto.replace("Mun","Calzada//")
+print(texto,nuevo_texto)
+print("Mundo"in texto)
+print(type(texto)) #con la palabra type () me esta diciendo que tipo de dato es el que estoy solicitando, en este caso me dice que es un string o sea un texto
+print(texto.capitalize()) #con capitalize coloca en mayusculas la primera letra de la primer palabra solamente, el resto quedara igual
+
+
+nombre=input("Coloque aqui su nombre ")
+edad=input("Coloque aqui su edad ")
+significado=("TE AMO")
+print(f"Sabe que su nombre {nombre} y su edad {edad} tienen un significado ")
+print("quieres saber cual es?")
+quiero=input ("Diga Si o No si desea saberlo ").lower()
+
+
+if quiero=='si':
+    print("TE AMO")
+else:
+    print("Lo lamento TE AMO igual jajajajaja")
