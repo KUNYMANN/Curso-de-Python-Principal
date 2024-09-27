009e36d9b6214dcb48db39a53babb116595b55c0
KUNYMANN <kunymann@hotmail.com>
2024-09-27 20:06:24 -0300

    terminado

--- VARIABLES/00-TODO STRING.py/prueva.py
+++ VARIABLES/00-TODO STRING.py/prueva.py
@@ -17,7 +17,7 @@ print(listado)
 print(len(listado))#cantidad de espacios utilizados, incluidos los espacios vacios y los caracteres especiales(,) TOTAL 21
 print("*".join(listado))#incerta entre caracter y caracter incluidos los espacios vacios el caracter seleccionado c*a*f*e*,* *t*e*,* *l*e*c*h*e*,* *a*g*u*a
 print(len("*".join(listado)))#cuenta espacios utilizados por la cadena comenzando de 0 incluidos los caracteres incertados con JOIN "c*a*f*e*,***t*e*,***l*e*c*h*e*,***a*g*u*a*" (TOTAL 41)
-print("*".join(listado).index("u"))#muestra en que ubicacion se encuentra el por primera vez el caracter solicitado incluidos los caracteres incertados con JOIN (UBICACION 38)
+print("*".join(listado).index("u"))#muestra en que ubicacion se encuentra el por primera vez el caracter solicitado, incluidos los caracteres incertados con JOIN (UBICACION 38)
 print(listado.split())#transforma una cadena en una lista utilizando los espacios vacios ['cafe,', 'te,', 'leche,', 'agua']
 print(len(listado.split()))#cuenta la cantidad de elementos de la lista (TOTAL 4)
 print(listado.index("a")) #muestra en que ubicacion se encuentra el por primera vez el caracter solicitado en este caso la "a" de c"a"fe
@@ -26,4 +26,5 @@ print(listado.index("e"))#muestra en que ubicacion se encuentra el por primera v
 print(listado.index("te"))#cuanto mas especifiquemos que estamos buscando mas certera sera la informacion y por ende va a modificar el indice que nos arroje
 'En el metodo INDEX si un valor solicitado no se encuentra, arroja error por consola, a diferencia del metodo FIND que arroja un valor -1 por consola'
 print(listado.split()[2])#arroja los objetos que se encuentran EN la posicion solicitada dentro de la lista creada con SPLIT ['leche'] contando desde 0
-print(listado.split()[0:2])#arroja los objetos que se encuentran "hasta" la posicion solicitada dentro de la lista creada con SPLIT ['cafe,', 'te,'] "no incluida" la segunda posicion[:02]
+print(listado.split()[0:2])#arroja los objetos que se encuentran desde / "hasta" la posicion solicitada dentro de la lista creada con SPLIT 
+                           #['cafe,', 'te,'] "sin incluir" el objeto solicitado en la segunda peticion [:02]
