81a5ea4003640e67be9e718f8b676e648841477b
KUNYMANN <kunymann@hotmail.com>
2024-12-01 23:41:49 -0300

    terminado

--- CURSO DE PYTHON CON EJERCICIOS y PRACTICAS/MI TUTORIAL PYTHON.PY/tempCodeRunnerFile.py
+++ CURSO DE PYTHON CON EJERCICIOS y PRACTICAS/MI TUTORIAL PYTHON.PY/tempCodeRunnerFile.py
@@ -1,26 +1,25 @@
 print(   "****   SISTEMA DE DESCUENTO    ****")
 monto_compra=float(input("Ingrese el monto total de su compra: "))
 membresia=input("Indique si/no es cliente de la tienda: ").strip().lower()
-descuento=monto_compra*10/100
-descuento1=monto_compra*5/100
-descuento2=monto_compra-descuento
-descuento3=monto_compra-descuento1
-descuento4=monto_compra*3/100
-descuento5=monto_compra-descuento4
-
 if monto_compra>=1000 and membresia=="si":
-        print(f"""Felicidades!!! por ser cliente y tu compra superar los $ 1.000 
+    descuento=monto_compra*10/100
+    descuento2=monto_compra-descuento
+    print(f"""Felicidades!!! por ser cliente y tu compra superar los $ 1.000 
     Te has hecho acreedor al siguiente descuento
     Monto de la compra: ........$ {monto_compra:.2f} 
     Descuento otorgado del 10%: $  {descuento:.2f}
     Tu monto a pagar es ........$ {descuento2:.2f}""")
 elif 1000>monto_compra>=500 and  membresia=="si":
+    descuento1=monto_compra*5/100
+    descuento3=monto_compra-descuento1
     print(f"""Por ser cliente pero tu compra no supera los $ 1.000 
     Te has hecho acreedor al siguiente descuento
     Monto de la compra:....... $ {monto_compra}
     Descuento otorgado del 5%: $  {descuento1:.2F} 
     Tu monto a pagar es ...... $ {descuento3:.2f}""")
 elif monto_compra<500 and membresia=="si":
+    descuento4=monto_compra*3/100
+    descuento5=monto_compra-descuento4
     print(f'''Por ser cliente 
     Te has hecho acreedor al siguiente descuento
     Monto de la compra: ........$ {monto_compra:.2f} 
