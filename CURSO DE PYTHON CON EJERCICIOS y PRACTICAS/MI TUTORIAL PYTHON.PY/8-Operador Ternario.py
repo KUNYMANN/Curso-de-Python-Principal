771a1c8b6f18d112b60ce8e53c7a747244ddf760
KUNYMANN <kunymann@hotmail.com>
2025-02-18 14:57:35 -0300

    Terminado

--- CURSO DE PYTHON CON EJERCICIOS y PRACTICAS/MI TUTORIAL PYTHON.PY/8-Operador Ternario.py
+++ CURSO DE PYTHON CON EJERCICIOS y PRACTICAS/MI TUTORIAL PYTHON.PY/8-Operador Ternario.py
@@ -91,8 +91,8 @@ vista al mar, así que eso es lo que debe de realizar su aplicación.'''
 
 print("    ***SISTEMA RESERVA HOTEL***")
 
-precio1=200
-precio2=150
+precio1=200 #habitacion CVM
+precio2=150 #habitacion SVM
 
 print(f"Cuartos con vista al mar $ {precio1} por dia")
 print(f"Cuartos sin vista al mar $ {precio2} por dia")
@@ -101,79 +101,76 @@ nombre=input("Ingrese su nombre y apellido completo: ").strip().title()
 dias=int(input("Indique la cantidad de dias que va a alojarse en el hotel: "))
 cuartos_vm=input("Desea un cuarto con vista al mar? (Si/No): ").strip().lower()
 efectivo_debito=input("Si abona en efectivo/debito obterndra un descuento 10% (Si/No): ").strip().lower()
-totalsdesc=precio1*dias
-totalsdesc1=precio2*dias
+masde=20
+
+
 print("\n----------Detalles de la reserva-----------")
+print()
+print(f"Nombre reserva: {nombre}")
+print(f"cantidad de dias: {dias}")
+
 
 #mas de 20 dias, Hab.C V Mar,pago en efectivo
 if dias>=20 and cuartos_vm=="si" and efectivo_debito=="si":
-   descuento1=precio1*.80 #20% mas de 20 dias hcvm
-   total_desc1=descuento1*dias #precio por la cantidad de dias alojados hcvm
-   descuento2=total_desc1*.90 #descuento 10 % pago efectivo
-   descuento3=precio2*.80 #20% mas de 20 dias hsvm
-   total_desc2=descuento3*dias #precio por la cantidad de dias alojados hsvm
-   print(f"Nombre reserva: {nombre}")
-   print(f"cantidad de dias: {dias}")
+   
+   costo_alojamiento1=precio1*dias # CVM  
+   descuento_efec=costo_alojamiento1*.80*.80 # 20% VM + 20% Efect
    print(f"Hab. vista al mar: {cuartos_vm}")
-   print(f"descuento mas 20 dias $ {descuento1}")
-   print(f"TOTAL con descuento efectivo: {descuento2} ")
+   print(f"Importe a abonar con descuento del {masde} % por alojarse mas de 20 dias $ {costo_alojamiento1*.80:.2f}")
+   print(f"Por abonar en efectivo su importe final es $ {descuento_efec:.2F} ")
    print(1)
 
 #mas de 20 dias, Hab.C V Mar,paga con otro medio  
-elif dias>=20 and cuartos_vm=="si" :
-   descuento1=precio1*.80*dias #20% mas de 20 dias hcvm
-   print(f"Nombre reserva: {nombre}")
-   print(f"cantidad de dias: {dias}")
+if dias>=20 and cuartos_vm=="si" and  efectivo_debito=="no":
+   costo_alojamiento1=precio1*dias # CVM 
    print(f"Hab. vista al mar: {cuartos_vm}")
-   print(f"total a pagar {descuento1}")
+   print(f"Importe a abonar con descuento del {masde} % por alojarse mas de 20 dias $ {costo_alojamiento1*.80:.2f}")
    print(2)
 
 #menos de 20 dias, hab. C V Mar, paga efectivo
-elif dias<20 and cuartos_vm=="si" and efectivo_debito=="si":
+if dias<20 and cuartos_vm=="si" and efectivo_debito=="si":
    descuento2=precio1*.90*dias #10% pago en efectivo
-   print(f"Nombre reserva: {nombre}")
-   print(f"cantidad de dias: {dias}")
    print(f"Hab. vista al mar: {cuartos_vm}")
-   print(f"total a pagar {descuento2}")
+   print(f"Por abonar en efectivo su importe final a pagar es $ {descuento2:.2f}")
    print(3)
 
 #menos de 20 dias, hab. C V Mar, paga con otro medio
-elif dias<20:
-   print(f"Nombre reserva: {nombre}")
-   print(f"cantidad de dias: {dias}")
+if dias<20 and cuartos_vm=="si" and efectivo_debito=="no" :
+   costo_alojamiento1=precio1*dias # CVM 
    print(f"Hab. vista al mar: {cuartos_vm}")
-   print(f"total a pagar $ {totalsdesc}")
-   print(4)
+   print(f"Total a pagar $ {costo_alojamiento1:.2f}")
+   print(4)   
 
-#mas 20 dias, hab. S V Mar, paga efectivo
-elif dias>=20 and efectivo_debito=="si":
-   descuento1=precio1*.80#20% mas de 20 dias hsvm
-   total_desc1=descuento1*dias #precio por la cantidad de dias alojados hcvm
-   descuento2=total_desc1*.90 #descuento 10 % pago efectivo
-   descuento3=precio2*.80 #20% mas de 20 dias hsvm
-   total_desc2=descuento3*dias #precio por la cantidad de dias alojados hsvm
-   print(f"Nombre reserva: {nombre}")
-   print(f"cantidad de dias: {dias}")
+#mas de 20 dias, hab. S V Mar, paga efectivo
+if dias>=20 and cuartos_vm=="no" and efectivo_debito=="si":
+   costo_alojamiento2=precio2*dias # SVM
+   descuento_efec=costo_alojamiento2*.80*.80 # 20% VM + 20% Efect
    print(f"Hab. vista al mar: {cuartos_vm}")
-   print(f"descuento mas 20 dias $ {descuento2}")
-   print(f"TOTAL con descuento efectivo: {total_desc2} ")
-   print(5)
-
-#menos de 20 dias, hab. S V Mar paga, paga efectivo
-elif dias<20 and efectivo_debito=="si":
-   descuento3=precio2*.80 #20% mas de 20 dias hsvm
-   total_desc2=descuento3*dias #precio por la cantidad de dias alojados hsvm
-   print(f"Nombre reserva: {nombre}")
-   print(f"cantidad de dias: {dias}")
+   print(f"Importe a abonar con descuento del {masde} % por alojarse mas de 20 dias $ {costo_alojamiento2*.80:.2f}")
+   print(f"Por abonar en efectivo su importe final es $ {descuento_efec:.2F} ")
+   print(5) 
+   
+#mas de 20 dias, hab. S V Mar, paga con otro medio
+if dias>=20 and cuartos_vm=="no" and efectivo_debito=="no":
+   costo_alojamiento2=precio2*dias # SVM
+   descuento_efec=costo_alojamiento2*.80*.80 # 20% VM + 20% Efect
    print(f"Hab. vista al mar: {cuartos_vm}")
-   print(f"TOTAL con descuento efectivo: {total_desc2} ")
-   print(6)
+   print(f"Importe a abonar con descuento del {masde} % por alojarse mas de 20 dias $ {costo_alojamiento2*.80:.2f}")
+   print(6) 
+   
+#menos de 20 dias, hab. S V Mar, paga efectivo
+if dias<20 and cuartos_vm=="no" and efectivo_debito=="si":
+   costo_alojamiento2=precio2*dias # SVM
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Por abonar en efectivo su importe final es $ {costo_alojamiento2*.80:.2F} ")
+   print(7) 
+   
+#mas de 20 dias, hab. S V Mar, paga con otro medio
+if dias<20 and cuartos_vm=="no" and efectivo_debito=="no":
+   costo_alojamiento2=precio2*dias # SVM
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Total a pagar $ {costo_alojamiento2:.2f}")
+   print(8) 
 
-#menos de 20 dias, hab. S V Mar, paga otros medios
-else:
-    
-    print(f"Nombre reserva: {nombre}")
-    print(f"cantidad de dias: {dias}")
-    print(f"Hab. vista al mar: {cuartos_vm}")
-    print(f"total a pagar $ {totalsdesc1}")
-    print(7)
+print(     '"GRACIAS POR ALOJARSE EN NUESTRO HOTEL"')
+
---
+++ CURSO DE PYTHON CON EJERCICIOS y PRACTICAS/MI TUTORIAL PYTHON.PY/reserva hotel alojamiento.py
@@ -0,0 +1,81 @@
+precio1=200 #habitacion CVM
+precio2=150 #habitacion SVM
+
+print(f"Cuartos con vista al mar $ {precio1} por dia")
+print(f"Cuartos sin vista al mar $ {precio2} por dia")
+print()
+nombre=input("Ingrese su nombre y apellido completo: ").strip().title()
+dias=int(input("Indique la cantidad de dias que va a alojarse en el hotel: "))
+cuartos_vm=input("Desea un cuarto con vista al mar? (Si/No): ").strip().lower()
+efectivo_debito=input("Si abona en efectivo/debito obterndra un descuento 10% (Si/No): ").strip().lower()
+masde=20
+
+
+print("\n----------Detalles de la reserva-----------")
+print()
+print(f"Nombre reserva: {nombre}")
+print(f"cantidad de dias: {dias}")
+
+
+#mas de 20 dias, Hab.C V Mar,pago en efectivo
+if dias>=20 and cuartos_vm=="si" and efectivo_debito=="si":
+   
+   costo_alojamiento1=precio1*dias # CVM  
+   descuento_efec=costo_alojamiento1*.80*.80 # 20% VM + 20% Efect
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Importe a abonar con descuento del {masde} % por alojarse mas de 20 dias $ {costo_alojamiento1*.80:.2f}")
+   print(f"Por abonar en efectivo su importe final es $ {descuento_efec:.2F} ")
+   print(1)
+
+#mas de 20 dias, Hab.C V Mar,paga con otro medio  
+if dias>=20 and cuartos_vm=="si" and  efectivo_debito=="no":
+   costo_alojamiento1=precio1*dias # CVM 
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Importe a abonar con descuento del {masde} % por alojarse mas de 20 dias $ {costo_alojamiento1*.80:.2f}")
+   print(2)
+
+#menos de 20 dias, hab. C V Mar, paga efectivo
+if dias<20 and cuartos_vm=="si" and efectivo_debito=="si":
+   descuento2=precio1*.90*dias #10% pago en efectivo
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Por abonar en efectivo su importe final a pagar es $ {descuento2:.2f}")
+   print(3)
+
+#menos de 20 dias, hab. C V Mar, paga con otro medio
+if dias<20 and cuartos_vm=="si" and efectivo_debito=="no" :
+   costo_alojamiento1=precio1*dias # CVM 
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Total a pagar $ {costo_alojamiento1:.2f}")
+   print(4)   
+
+#mas de 20 dias, hab. S V Mar, paga efectivo
+if dias>=20 and cuartos_vm=="no" and efectivo_debito=="si":
+   costo_alojamiento2=precio2*dias # SVM
+   descuento_efec=costo_alojamiento2*.80*.80 # 20% VM + 20% Efect
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Importe a abonar con descuento del {masde} % por alojarse mas de 20 dias $ {costo_alojamiento2*.80:.2f}")
+   print(f"Por abonar en efectivo su importe final es $ {descuento_efec:.2F} ")
+   print(5) 
+   
+#mas de 20 dias, hab. S V Mar, paga con otro medio
+if dias>=20 and cuartos_vm=="no" and efectivo_debito=="no":
+   costo_alojamiento2=precio2*dias # SVM
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Importe a abonar con descuento del {masde} % por alojarse mas de 20 dias $ {costo_alojamiento2*.80:.2f}")
+   print(6) 
+   
+#menos de 20 dias, hab. S V Mar, paga efectivo
+if dias<20 and cuartos_vm=="no" and efectivo_debito=="si":
+   costo_alojamiento2=precio2*dias # SVM
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Por abonar en efectivo su importe final es $ {costo_alojamiento2*.80:.2F} ")
+   print(7) 
+   
+#mas de 20 dias, hab. S V Mar, paga con otro medio
+if dias<20 and cuartos_vm=="no" and efectivo_debito=="no":
+   costo_alojamiento2=precio2*dias # SVM
+   print(f"Hab. vista al mar: {cuartos_vm}")
+   print(f"Total a pagar $ {costo_alojamiento2:.2f}")
+   print(8) 
+
+print(     '"GRACIAS POR ALOJARSE EN NUESTRO HOTEL"')
